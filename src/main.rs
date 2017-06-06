24760adc2bee368ffbf37e7441ed65f62ef28e3d
Shamir Khodzha <khodzha.sh@gmail.com>
2017-06-06 22:54:20 +0300

    simple executable to calc perceptual hashes of image provided as first arg

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate image;
+extern crate img_hash;
+#[macro_use]
+extern crate log;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+use img_hash::{ImageHash, HashType};
+
+fn main() {
+    let path: String = env::args().nth(1).unwrap();
+    match phash(&path) {
+        Ok(hash) => {
+            println!("{}", hash.to_base64());
+        }
+        Err(e) => {
+            error!("Error: {}! Filepath: {}", e, path);
+        }
+    }
+
+}
+
+fn phash(path: &String) -> Result<ImageHash, String> {
+
+    let mut file = try!(File::open(path).map_err(|e| e.to_string()));
+    let mut buffer: Vec<u8> = Vec::new();
+    try!(file.read_to_end(&mut buffer).map_err(|e| e.to_string()));
+    let image = try!(image::load_from_memory(&buffer).map_err(|e| e.to_string()));
+
+    let hash = ImageHash::hash(&image, 8, HashType::Gradient);
+    Ok(hash)
+}
