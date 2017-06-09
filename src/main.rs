1c2c21d34ab4f145efc1f6c6a970d12efc0272e2
Shamir Khodzha <khodzha.sh@gmail.com>
2017-06-09 23:33:22 +0300

    added md5 hash of pixels

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate image;
 extern crate img_hash;
+extern crate crypto;
 extern crate serde;
 #[macro_use]
 extern crate serde_json;
@@ -8,6 +9,9 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::collections::HashMap;
+
+use crypto::digest::Digest;
+use crypto::md5::Md5;
 
 use img_hash::{ImageHash, HashType};
 
@@ -18,9 +22,20 @@ fn main() {
     let mut file = File::open(path).unwrap();
     let mut buffer: Vec<u8> = Vec::new();
     file.read_to_end(&mut buffer);
+
+    let mut hash_result: Vec<u8> = Vec:: with_capacity(16);
+    for _ in 0..16 {
+        hash_result.push(0);
+    }
+    let mut hash = Md5::new();
+
     let image = image::load_from_memory(&buffer).unwrap();
 
+    hash.input(&image.raw_pixels());
+    hash.result(&mut hash_result);
+
     let mut hashes = HashMap::new();
+    hashes.insert("md5", hash_result);
     hashes.insert("mean", ImageHash::hash(&image, HASH_SIZE, HashType::Mean).bitv.to_bytes());
     hashes.insert("gradient", ImageHash::hash(&image, HASH_SIZE, HashType::DoubleGradient).bitv.to_bytes());
     hashes.insert("dct", ImageHash::hash(&image, HASH_SIZE, HashType::DCT).bitv.to_bytes());
